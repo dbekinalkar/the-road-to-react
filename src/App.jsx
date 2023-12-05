efba5144ea053284eb6183639f354c114d5d9232
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-12-05 14:31:43 -0500

    Utilize JavaScript object for greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 
-const title = 'React';
+const welcome = {
+  greeting: "Hello",
+  title: "React"
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
 
 
       <label htmlFor="search">Search: </label>
