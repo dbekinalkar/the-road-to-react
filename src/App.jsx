6f63fbe4504a2805c0b704cf888281425821dd6c
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-12-06 17:38:38 -0500

    Implement List as Separate React Component

--- src/App.jsx
+++ src/App.jsx
@@ -34,19 +34,25 @@ function App() {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
 
-      <ul>
-        {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        ))}
-      </ul>
+      <List />
     </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      ))}
+    </ul>
   );
 }
 
