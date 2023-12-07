60e86186435997edb6116d8e9db8e6cc95c5f4ec
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-12-06 20:19:24 -0500

    Implement Components using Props

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,26 @@ const welcome = {
   title: "React",
 };
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div>
       <h1>
@@ -33,10 +33,10 @@ const App = () => {
 
       <Search />
 
-      <List />
+      <List list={stories} />
     </div>
   );
-}
+};
 
 const Search = () => {
   const handleChange = (event) => {
@@ -44,31 +44,37 @@ const Search = () => {
     console.log(event);
     // value of target (here: input HTML element)
     console.log(event.target.value);
-  }
+  };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
-}
+};
 
-const List = () => {
+const List = (props) => {
   return (
     <ul>
-      {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
       ))}
     </ul>
   );
-}
+};
+
+const Item = (props) => {
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
 
 export default App;
