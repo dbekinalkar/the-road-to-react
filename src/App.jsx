9fc6388022e2db00f687064abda5914f25de1e67
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-12-17 21:50:17 -0600

    Implement API call for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import "./App.css";
 import * as React from "react";
 
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
 const welcome = {
   greeting: "Hello",
   title: "React",
 };
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+// const initialStories = [
+//   {
+//     title: "React",
+//     url: "https://reactjs.org/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: "Redux",
+//     url: "https://redux.js.org/",
+//     author: "Dan Abramov, Andrew Clark",
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+// ];
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -35,10 +37,10 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-  );
+// const getAsyncStories = () =>
+//   new Promise((resolve) =>
+//     setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
+//   );
 
 const App = () => {
   const [stories, dispatchStories] = React.useReducer(
@@ -76,11 +78,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
