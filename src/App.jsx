6230e0e7d8041f9653ce2f9e1c3ab48a8ef142e8
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-12-17 21:04:34 -0600

    Implement story logic using reducer logic

--- src/App.jsx
+++ src/App.jsx
@@ -41,26 +41,41 @@ const getAsyncStories = () =>
   );
 
 const App = () => {
-  const [stories, setStories] = React.useState([]);
+  const [stories, dispatchStories] = React.useReducer((state, action) => {
+    switch (action.type) {
+      case "SET_STORIES":
+        return action.payload;
+      case "REMOVE_STORY":
+        return state.filter(
+          (story) => action.payload.objectID !== story.objectID
+        );
+      default:
+        throw new Error();
+    }
+  }, []);
 
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
     setIsLoading(true);
+
     getAsyncStories()
       .then((result) => {
-        setStories(result.data.stories);
+        dispatchStories({
+          type: "SET_STORIES",
+          payload: result.data.stories,
+        });
         setIsLoading(false);
       })
       .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-    setStories(newStories);
+    dispatchStories({
+      type: "REMOVE_STORY",
+      payload: item,
+    });
   };
 
   const [searchTerm, setSearchTerm] = useStorageState("search", "");
