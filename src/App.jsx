c6f727a81d8cb0f20e24bf51a7a54f8edf0fd90a
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-12-19 22:49:25 -0600

    Implement data fetch with callback handler

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
@@ -96,6 +96,10 @@ const App = () => {
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
