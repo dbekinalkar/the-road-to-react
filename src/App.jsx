8429eeb48a795473426889158bc57789a7216a11
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-12-20 12:13:07 -0600

    Implement HTML form

--- src/App.jsx
+++ src/App.jsx
@@ -125,17 +125,11 @@ const App = () => {
         {welcome.greeting} {welcome.title}
       </h1>
 
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        onInputChange={handleSearchInput}
-      >
-        Search:
-      </InputWithLabel>
-
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       {stories.isError && <p>An error occured</p>}
 
@@ -147,6 +141,23 @@ const App = () => {
     </>
   );
 };
+
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({
   id,
